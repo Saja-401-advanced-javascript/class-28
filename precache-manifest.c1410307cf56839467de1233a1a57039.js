530e7d2fa9dc5839ee1efdadfe8b7f40346799b2
SajaSwalgah <runner@fv-az50.c3kqlw5xrv1ezdhdg4bw5343ia.cx.internal.cloudapp.net>
2020-03-03 11:02:27 +0000

    Updates

---
+++ precache-manifest.c1410307cf56839467de1233a1a57039.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460d5871a82fec67ab3ec665cddc29ec",
+    "url": "/Saja-401-advanced-javascript/class-28/index.html"
+  },
+  {
+    "revision": "ee9e43b1d84ec2142ea1",
+    "url": "/Saja-401-advanced-javascript/class-28/static/css/main.76b75f2b.chunk.css"
+  },
+  {
+    "revision": "a1213bc6e149e4063897",
+    "url": "/Saja-401-advanced-javascript/class-28/static/js/2.b8955670.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Saja-401-advanced-javascript/class-28/static/js/2.b8955670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9e43b1d84ec2142ea1",
+    "url": "/Saja-401-advanced-javascript/class-28/static/js/main.fa50690d.chunk.js"
+  },
+  {
+    "revision": "a62224daba0b01f6e616",
+    "url": "/Saja-401-advanced-javascript/class-28/static/js/runtime-main.983226b0.js"
+  }
+]);
